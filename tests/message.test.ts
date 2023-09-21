89c228fb0730d0f7ba569028ae3c6c68e8b3cc00
Miles Stötzner <miles@stoetzner.de>
2023-09-21 21:44:29 +0200

    some from-to tests

---
+++ tests/message.test.ts
@@ -0,0 +1,10 @@
+import Message from '#core/message'
+import {expect} from 'chai'
+
+describe.only('message', () => {
+    it('from-to', async () => {
+        const message = Message.fromJSON({id: 69, data: [1, 2, 3], ext: false, rtr: false})
+        const result = Message.fromJSON(Message.fromCAN(Message.fromString(message.toString()).toCAN()).toJSON())
+        expect(result.toString()).to.equal(message.toString())
+    })
+})
